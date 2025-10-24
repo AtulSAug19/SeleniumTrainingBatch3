bf2b23190078621143f6fc75f9c896f28d58cc12
Kushwah <yogesh.b.kushwah@capgemini.com>
2025-10-24 16:46:37 +0530

    change

--- src/test/java/seleniumEx/Selenium.java
+++ src/test/java/seleniumEx/Selenium.java
@@ -6,6 +6,8 @@ public class Selenium {
   int a = 202;
   int c = 323;
   public static void main(String[] args) {
-	System.out.println(123);
+	System.out.println("asdfasd");
+	  System.out.println(123);
+	
 }
 }
