dfb12071fd385c8149536846aad3138f80c8789d
Kushwah <yogesh.b.kushwah@capgemini.com>
2025-10-24 16:51:40 +0530

    change

--- src/test/java/seleniumEx/Selenium.java
+++ src/test/java/seleniumEx/Selenium.java
@@ -1,15 +1,13 @@
 package seleniumEx;
 
 public class Selenium {
-
+	public static void main(String[] a) {
+		System.out.println("hih");
+		System.out.println("hih");
+		System.out.println("hih");
   int b =10;
   int a = 202;
   int c = 323;
-  public static void main(String[] args) {
-	int a =192;
-	int c = asdf;
-	
-	  System.out.println(123);
-	
-}
+	}
+ 
 }
