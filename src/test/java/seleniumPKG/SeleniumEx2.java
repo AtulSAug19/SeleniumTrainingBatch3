28fa5bbc0d1a59a41c007b2fd7b6c2d057051a00
Rajkumar Dhakate <narendra.rajkumar-dhakate@capgemini.com>
2025-10-26 18:48:56 +0530

    narendra first commit

---
+++ src/test/java/seleniumPKG/SeleniumEx2.java
@@ -0,0 +1,11 @@
+package seleniumPKG;
+
+public class SeleniumEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, from narendra");
+
+	}
+
+}
